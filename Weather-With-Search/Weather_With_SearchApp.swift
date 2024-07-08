ab25b1707b85b3d05c34cf7ac02100843569cd05
Yumeng Liu <lym20041026@gmail.com>
2024-07-07 19:43:40 -0700

    Initial Commit

---
+++ Weather-With-Search/Weather_With_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_With_SearchApp.swift
+//  Weather-With-Search
+//
+//  Created by Yumeng Liu on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_With_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
